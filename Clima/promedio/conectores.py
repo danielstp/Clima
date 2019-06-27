ed3554cef5e0ea6436761f044a54ee881d76ab73
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2019-06-27 20:45:41 +0000

    funcionando

--- Clima/promedio/conectores.py
+++ Clima/promedio/conectores.py
@@ -20,7 +20,7 @@ def accuweather(lat, lon):
   response = requests.get('http://localhost:5000/accuweather', headers=headers, params=params)
   a = response.json()
 
-  return aa['simpleforecast']['forecastday'][0]['current']['celsius']
+  return float(a['simpleforecast']['forecastday'][0]['current']['celsius'])
 
 def noaa(lat, lon ):
   headers = {
@@ -38,19 +38,20 @@ def noaa(lat, lon ):
 
   response = requests.get('http://localhost:5000/noaa', headers=headers, params=params)
   a = response.json()
-  return a['today']['current']['celsius']
+  return float(a['today']['current']['celsius'])
 
 def weatherdotcom(lat, lon ):
   headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept': 'application/json',
     'Accept-Language': 'en-US,en;q=0.5',
+    'Content-Type': 'application/json',
     'DNT': '1',
     'Connection': 'keep-alive',
     'Upgrade-Insecure-Requests': '1',
   }
 
-  data = {'lat': str( lat ),'lon': str( lon ) }
+  data = '{"lat": ' + str( lat ) +', "lon": ' + str( lon ) + ' }'
 
   response = requests.post('http://localhost:5000/weatherdotcom', headers=headers, data=data)
-  return response
+  return float(response.json()['query']['results']['channel']['condition']['temp'])
--- Clima/promedio/views.py
+++ Clima/promedio/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-def promedio( self, lat, lon, servicios = "noaa,weather,accu" ):
+def promedio( self, lat, lon, servicios = "noaa,accuweather,weatherdotcom" ):
     suma = 0
     n = 0
     from promedio.conectores import noaa, accuweather, weatherdotcom
     for servicio in [x.strip() for x in servicios.split(',')]:
         if( servicio == "accuweather"):
             n = n + 1
-            sumaLat = suma + accuweather( lat=lat, lon=lon )
+            suma = suma + accuweather( lat=lat, lon=lon )
+            print(suma)
+            print(n)
             continue
         elif( servicio == "noaa" ):
             n = n + 1
             suma = suma + noaa( lat=lat, lon=lon )
+            print(suma)
+            print(n)
             continue
         elif( "weatherdotcom" ):
             n = n + 1
             suma = suma + weatherdotcom( lat=lat, lon=lon )
+            print(suma)
+            print(n)
             continue
         else:
             return JsonResponse({'error':'Parametros equivocados'})
