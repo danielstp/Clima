e10b9c8f11b26b3e251e9e48ca89f0fe7485d595
Daniel Saguez Tezanos Pinto <daniel.saguez@salamancasolutions.com>
2019-06-28 11:18:29 -0400

    Con caracteristicas mejoradas Heroku Travis y otros

--- clima/promedio/views.py
+++ clima/promedio/views.py
@@ -4,6 +4,11 @@ from django.http import JsonResponse
 def promedio( self, lat, lon, servicios = "noaa,accuweather,weatherdotcom" ):
     suma = 0
     n = 0
+    try:
+       lat = float(lat)
+       lon = float(lon)
+    except:
+       return JsonResponse({'error':'La longitud y la latitud deben ser nuemros, y pueden tener punto decimal'})
     from promedio.conectores import noaa, accuweather, weatherdotcom
     for servicio in set([x.strip() for x in servicios.split(',')]):
         if( servicio == "accuweather"):
